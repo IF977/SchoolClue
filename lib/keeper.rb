62ccd4c3d4bf6be3c6cf1754dbe8eda6572a659d
David <dfnb@cin.ufpe.br>
2017-05-22 19:53:49 +0000

    correção de tdd de keeper

---
+++ lib/keeper.rb
@@ -0,0 +1,11 @@
+class Keeper
+    attr_accessor :lat, :lon, :name, :phone, :email
+    def initialize
+        @lat = 1.2
+        @lon = 1.17
+        @email = "escolinha_do_senhor_raimundo@escolinha.com.br"
+        @name = "escolinha do senhor raimundo"
+        @phone = "3456435"
+    end
+    
+end
